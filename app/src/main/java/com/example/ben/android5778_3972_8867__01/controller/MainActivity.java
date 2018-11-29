382e1923415ab5157786154ba854b7e7701c3aff
code613 <benmarcus724@gmail.com>
2018-11-29 14:01:53 +0200

    added stuff

--- app/src/main/java/com/example/ben/android5778_3972_8867__01/controller/MainActivity.java
+++ app/src/main/java/com/example/ben/android5778_3972_8867__01/controller/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.ben.android5778_3972_8867__01.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
--- app/src/main/java/com/example/ben/android5778_3972_8867__01/model/entities/Client.java
+++ app/src/main/java/com/example/ben/android5778_3972_8867__01/model/entities/Client.java
@@ -5,4 +5,48 @@ public class Client {
     String clientName;
     String clientPhoneNumber;
     String clientEmailAddress;
+
+    //***********************  constructor ***********************//
+    public Client(String clientName, String clientPhoneNumber, String clientEmailAddress) {
+        this.clientName = clientName;
+        this.clientPhoneNumber = clientPhoneNumber;
+        this.clientEmailAddress = clientEmailAddress;
+    }
+
+    //***********************  getters / setters ***********************//
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientPhoneNumber() {
+        return clientPhoneNumber;
+    }
+
+    public void setClientPhoneNumber(String clientPhoneNumber) {
+        this.clientPhoneNumber = clientPhoneNumber;
+    }
+
+    public String getClientEmailAddress() {
+        return clientEmailAddress;
+    }
+
+    public void setClientEmailAddress(String clientEmailAddress) {
+        this.clientEmailAddress = clientEmailAddress;
+    }
+
+    //***********************  implementations ***********************//
+    @Override
+    public String toString() {
+        return "Client{" +
+                "clientName='" + clientName + '\'' +
+                ", clientPhoneNumber='" + clientPhoneNumber + '\'' +
+                ", clientEmailAddress='" + clientEmailAddress + '\'' +
+                '}';
+    }
+
+
 }
